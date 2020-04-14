2c9bc0f949de948a6ee80cebc7232bf8295011fa
Andres Carrillo Lopez <acarrillolopez98@gmail.com>
2020-04-15 00:12:30 +0200

    added init_arxiv_parser and first version of arxiv querier

--- lib/doc_utils.py
+++ lib/doc_utils.py
@@ -17,6 +17,10 @@
 import wikipedia
 import wikipediaapi
 
+# Arxiv parser
+from bs4 import BeautifulSoup
+import urllib.request
+
 # Data processing
 import numpy as np
 import nltk, gensim
@@ -520,3 +524,79 @@ def evaluate_model(model, test_corpus, test_labels, eval="binary"):
     print("Model {} accuracy over {} test documents: {}%.".format(eval, len(test_labels), np.mean(accuracy_list) * 100))
 
     return predictions, accuracy_list
+
+def init_arxiv_parser(test_size = 50):
+    '''
+    Auxiliary function for initializing the arxiv parser.
+    Returns the queries (urls) feeded later to the parser.
+    '''
+    if test_size not in [25,50,100,200]:
+        #Arxiv url query only allows [25, 50, 100, 200]
+        print("[FAILED] Allowed test_sizes (arxiv requirement): 25, 500, 100 or 200.") 
+        return -1
+
+    subjects = ["computer_science",
+                "economics",
+                "eess",
+                "mathematics",
+                "physics",
+                "q_biology",
+                "q_finance",
+                "statistics"]
+
+    queries = list()
+
+    #query parts for readability:
+    base = "https://arxiv.org/search/advanced?advanced="
+    q1 = "terms-0-operator=AND&terms-0-term=&terms-0-field=title"
+    q2 = "classification-physics_archives=all&classification-include_cross_list=exclude"
+    q3 = "date-year=&date-filter_by=date_range&date-from_date=2010&date-to_date=2018&date-date_type=submitted_date"
+    q4 = "abstracts=show&size="+str(test_size)+"&order=-announced_date_first"
+
+    for subject in subjects:
+        subject_query = "{b}&{q1}&classification-{subject}=y&{q2}&{q3}&{q4}".format(b=base,q1=q1,subject=subject,q2=q2,q3=q3,q4=q4)
+        queries.append(subject_query)
+
+    return queries
+
+def arxiv_parser(test_size):
+    '''
+    Test_size: number of articles per topic to obtain.
+    Returns list of subjects each with papers of a certain topic.
+    '''
+    queries = init_arxiv_parser(test_size)
+    if queries == -1: #Query not accepted
+        return None 
+
+    arxiv_soups = list() #Will store here the BS4 formatted responses
+    
+    for query in queries:
+        arxiv_request = urllib.request.urlopen(query)
+        arxiv_response = arxiv_request.read()
+        arxiv_soups.append(BeautifulSoup(arxiv_response, 'html.parser'))
+
+    papers_dataset = list()
+
+    for subject in arxiv_soups:
+        subject_xml_contents = list()
+        for result in subject.find_all("li", {"class": "arxiv-result"}):
+            subject_xml_contents.append(result)
+    
+        subject_papers = list()
+
+        for result in subject_xml_contents:
+            raw_title = result.find_all("p", {"class": "title is-5 mathjax"})[0].getText()
+            title = " ".join(raw_title.split())
+            raw_abstract = result.find_all("span", {"class": "abstract-full has-text-grey-dark mathjax"})[0].getText()
+            abstract = " ".join(raw_abstract.split())
+            paper = {"title":title, "abstract":abstract}
+            subject_papers.append(paper)
+        
+        papers_dataset.append(subject_papers)
+
+    data_size = 0
+    for subject in papers_dataset:
+        data_size += len(subject)
+    print("Retrieved {} papers in total from {} subjects ({} from each).".format(data_size,len(papers_dataset), int(data_size/len(papers_dataset))))
+
+    return papers_dataset
