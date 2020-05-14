b4036a20e70474587caeba66111fefa81bb5a547
Andres Carrillo Lopez <acarrillolopez98@gmail.com>
2020-05-14 16:55:40 +0200

    Implemented naive label prop for Wiki dataset

--- lib/max_sim_classifier.py
+++ lib/max_sim_classifier.py
@@ -125,29 +125,89 @@ class MaxSimClassifier(ClassifierMixin, BaseEstimator):
         # print("Model {} accuracy over {} test documents: {}%.".format(eval, len(y), np.mean(accuracy_list) * 100))
         return np.mean(accuracy_list)
 
-    # TODO: Get topn instead of top1
-    # TODO: Change so that instead of need to create new model, "refits" the actual model
-    def label_prop(self, x_train, dataset,paperslist ,result="extended" ,top_n=2, debug = False ):
-        """        
-        Given a set of topic definitions and documents (x_train, dataset), 
-        it performs inference by comparing docs against the topic definitions 
-        and obtain the most similar paper/s per topic. 
+    def label_prop(self, x_train, dataset, paperslist = None ,result="extended" ,top_n=1, debug = False ):
+            """        
+            Given a set of topic definitions and documents (x_train, dataset), 
+            it performs inference by comparing docs against the topic definitions 
+            and obtain the most similar papers/articles predicted per topic. 
 
-        Currently only supports top1 (i.e best paper) addition.
+            Currently only supports top1 (i.e best prediction) addition.
 
-        Resulting new training data either consisting of:
-            - Original definitions + best paper/s
-            - best paper/s matching the topic
-        """
-        if self.dataset_type not in  "arxiv":
-            print("label propagation only supported for arxiv dataset")
-            #TODO: Add Wiki dataset support.
-            return -1
+            Resulting new training data either consisting of:
+                - Original definitions + best article/paper
+                - best article/paper matching the topic
+            """
+        if "arxiv" in self.dataset_type:
+            return self.arxiv_label_prop(x_train, dataset, paperslist ,result ,top_n, debug )
+        else: #wiki_dataset
+            return self.wiki_label_prop(x_train, dataset, result, top_n, debug )
 
-        input_articles = list(doc_utils.prepare_corpus(dataset, train_data=False, 
-                                                       preprocess=self.preprocess,dataset_type=self.dataset_type))
+    def arxiv_label_prop(self, x_train, dataset,paperslist ,result="extended" ,top_n=1, debug = False ):
 
-        doc_topics_sims = [ [],[],[],[],[],[],[],[]]
+            if paperslist is None:
+                print("ERROR: paperlist from Arxivparser needed")
+                return -1
+
+            input_articles = list(doc_utils.prepare_corpus(dataset, train_data=False, 
+                                                        preprocess=self.preprocess,dataset_type=self.dataset_type))
+
+            doc_topics_sims = [ [],[],[],[],[],[],[],[]]
+
+            for doc_id, doc in enumerate(input_articles):
+                inferred_vector = self.model.infer_vector(doc)
+                sims = self.model.docvecs.most_similar([inferred_vector], topn=len(self.model.docvecs))
+                top_n_sims = sims[:top_n]
+
+                for i in range(top_n):
+                    topic_id = top_n_sims[i][0] #predicted topic id of paper
+                    topic_sim = top_n_sims[i][1] #similarity
+                    doc_topics_sims[topic_id].append((topic_sim, doc_id))
+
+            best_papers_per_topic = [-1,-1,-1,-1,-1,-1,-1,-1]
+
+            n_papers_per_topic = len(paperslist)//len(doc_utils.ARXIV_WIKI_TOPICS)
+
+            for i,topic in enumerate(doc_topics_sims):
+                paper_id = (max(topic, key = lambda i : i[0])[1])
+                best_papers_per_topic[i] = paper_id #TODO: add topN papers and not just best one
+
+                if debug:
+                    true_label = paper_id//n_papers_per_topic
+                    print("Topic {} ({}) best matching paper: id #{}".format(i,doc_utils.ARXIV_WIKI_TOPICS[i],paper_id))
+                    print("\t--->True label:[",str(true_label), "](",doc_utils.ARXIV_WIKI_TOPICS[true_label] ,
+                            ") \t\tPaper title:",paperslist[paper_id]['title'])
+
+            x_train_ext = ["", "", "", "", "", "", "", ""]
+            x_train_papers = ["", "", "", "", "", "", "", ""]
+
+            for topic_id, train_sample in enumerate(x_train):
+                best_paper_id = best_papers_per_topic[topic_id]
+                #Creating extended train data
+                if result in "extended":
+                    x_train_ext[topic_id] = " . ".join([train_sample, paperslist[best_paper_id]["title"], paperslist[best_paper_id]["abstract"]])
+                elif result in "bestpapers": #train samples are the best papers
+                    x_train_papers[topic_id] = paperslist[best_paper_id]["title"] + " : " +paperslist[best_paper_id]["abstract"] 
+                else:
+                    print("[ERROR] Result argument can be only 'extended' or 'bestpapers'.")
+                    return -1
+
+            if result in "extended":
+                new_x_train = x_train_ext
+            else: #best papers
+                new_x_train = x_train_papers
+
+            return dataset, new_x_train
+
+    def wiki_label_prop(self, x_train, dataset,result="extended" ,top_n=2, debug = False ):
+
+        input_articles = list(doc_utils.prepare_corpus(dataset, train_data=False,preprocess=self.preprocess,dataset_type=self.dataset_type))
+
+        doc_topics_sims = [ [],[],[],[],[],[],[],[],[]]
+        
+        article_list = list()
+        for topic in dataset:
+            for article in topic[0]:
+                article_list.append(article)
 
         for doc_id, doc in enumerate(input_articles):
             inferred_vector = self.model.infer_vector(doc)
@@ -159,54 +219,34 @@ class MaxSimClassifier(ClassifierMixin, BaseEstimator):
                 topic_sim = top_n_sims[i][1] #similarity
                 doc_topics_sims[topic_id].append((topic_sim, doc_id))
 
-        best_papers_per_topic = [-1,-1,-1,-1,-1,-1,-1,-1]
-
-        n_papers_per_topic = len(paperslist)//len(doc_utils.ARXIV_WIKI_TOPICS)
+        best_articles_per_topic = [-1,-1,-1,-1,-1,-1,-1,-1,-1]
 
         for i,topic in enumerate(doc_topics_sims):
-            paper_id = (max(topic, key = lambda i : i[0])[1])
-            best_papers_per_topic[i] = paper_id #TODO: add topN papers and not just best one
+            article_id = (max(topic, key = lambda i : i[0])[1])
+            if debug: 
+                print("For topic {}, best article id: {}".format(i, article_id))
+            best_articles_per_topic[i] = article_id #TODO: add topN papers and not just best one
 
-            if debug:
-                true_label = paper_id//n_papers_per_topic
-                print("Topic {} ({}) best matching paper: id #{}".format(i,doc_utils.ARXIV_WIKI_TOPICS[i],paper_id))
-                print("\t--->True label:[",str(true_label), "](",doc_utils.ARXIV_WIKI_TOPICS[true_label] ,
-                        ") \t\tPaper title:",paperslist[paper_id]['title'])
+        x_train_ext = ["", "", "", "", "", "", "", "", ""]
+        x_train_articles = ["", "", "", "", "", "", "", "", ""]
 
-        x_train_ext = ["", "", "", "", "", "", "", ""]
-        x_train_papers = ["", "", "", "", "", "", "", ""]
-        print(" ")
         for topic_id, train_sample in enumerate(x_train):
-            best_paper_id = best_papers_per_topic[topic_id]
+            best_article_id = best_articles_per_topic[topic_id]
+            if debug:
+                print("For topic id {}, Adding article id {} to dataset".format(topic_id,best_article_id))
+                print(article_list[best_article_id])
             #Creating extended train data
-            if result in "extended":
-                x_train_ext[topic_id] = " . ".join([train_sample, paperslist[best_paper_id]["title"], paperslist[best_paper_id]["abstract"]])
-            elif result in "bestpapers": #train samples are the best papers
-                x_train_papers[topic_id] = paperslist[best_paper_id]["title"] + " : " +paperslist[best_paper_id]["abstract"] 
+            if result in "extended":    
+                x_train_ext[topic_id] = " . ".join([article_list[best_article_id],train_sample])
+            elif result in "bestpapers": #train samples are the best articles
+                x_train_articles[topic_id] = article_list[best_article_id]
             else:
                 print("[ERROR] Result argument can be only 'extended' or 'bestpapers'.")
                 return -1
 
-        #Removed "new y", as we leave the papers in the test with their original (true) labels
-        #new_y_test = list() #recreating original y test
-        #for doc_id, _ in enumerate(paperslist):
-        #    label = doc_id//n_papers_per_topic
-        #    new_y_test.append(label)
-
-        #for id_paper_to_remove in best_papers_per_topic:
-        #    topic_label = id_paper_to_remove//n_papers_per_topic
-        #    paper_topic_id = (id_paper_to_remove % n_papers_per_topic)
-        #    if debug:
-        #        print("Removing paper #{} (local #{}) from dataset (topic #{}).".format(id_paper_to_remove,
-        #                                                                            paper_topic_id,
-        #                                                                            topic_label))
-        #    #Removing the used papers from test data
-        #    del(dataset[topic_label]["papers"][paper_topic_id])
-        #    del(new_y_test[id_paper_to_remove])
-
         if result in "extended":
             new_x_train = x_train_ext
-        else: #best papers
-            new_x_train = x_train_papers
+        else: #best articles
+            new_x_train = x_train_articles
 
-        return dataset, new_x_train
+        return dataset ,new_x_train
